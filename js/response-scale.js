170510dac92765efaf157be6e6fb776c1e70ccef
naga2hands <141583323+naga2hands@users.noreply.github.com>
2026-07-23 12:50:57 +0000

    ss fix

---
+++ js/response-scale.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", () => {
+
+    if (window.innerWidth <= 1920) {
+        document.body.style.zoom = "67%";
+    } else {
+        document.body.style.zoom = "100%";
+    }
+
+});
