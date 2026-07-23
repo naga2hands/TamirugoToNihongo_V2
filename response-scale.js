44b117e62569a631ec2b477c2e12049251ff3154
naga2hands <141583323+naga2hands@users.noreply.github.com>
2026-07-23 15:56:28 +0000

    side bar

--- response-scale.js
+++ response-scale.js
@@ -4,9 +4,13 @@ window.addEventListener("load", () => {
     if (window.innerWidth <= 1920) {
         document.querySelector(".module-content").style.zoom = "67%";
         console.log("Applied 67% zoom1");
+                    "--sidebar-min-width",
+                    "140px"
     } else {
         document.querySelector(".module-content").style.zoom = "100%";
         console.log("Applied 100% zoom1");
+                    "--sidebar-max-width",
+                    "180px"
     }
 
     console.log("response-scale.js loaded");
