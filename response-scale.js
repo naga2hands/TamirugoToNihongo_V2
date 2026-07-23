39214403d1ca8095b7cd282319818574b7ef08c8
naga2hands <ngrjillution2892@gmail.com>
2026-07-24 01:03:35 +0530

    m

--- response-scale.js
+++ response-scale.js
@@ -10,7 +10,7 @@ window.addEventListener("load", () => {
 
   if (mc && sb) {
     if (w <= 1366 || dpr >= 1.25) {
-      mc.style.zoom = "67%";
+      mc.style.zoom = "65%";
       sb.style.zoom = "80%";
       console.log("Applied laptop scale");
     } else {
