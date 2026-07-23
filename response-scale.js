66c4ce34dac982fbd97c73ab4a9f3e878d9b167b
naga2hands <141583323+naga2hands@users.noreply.github.com>
2026-07-23 16:08:43 +0000

    zz

--- response-scale.js
+++ response-scale.js
@@ -3,12 +3,11 @@ window.addEventListener("load", () => {
 
     if (window.innerWidth <= 1920) {
         document.querySelector(".module-content").style.zoom = "67%";
-        console.log("Applied 67% zoom1");
         document.querySelector(".sidebar").style.fontSize = "85%";
+        console.log("Applied 67% zoom2");
     } else {
         document.querySelector(".module-content").style.zoom = "100%";
-        console.log("Applied 100% zoom1");
         document.querySelector(".sidebar").style.fontSize = "100%";    }
-
+        console.log("Applied 100% zoom2");
     console.log("response-scale.js loaded");
 });
